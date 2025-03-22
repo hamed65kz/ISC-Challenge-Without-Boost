2ae98b22f963efa55dca2528598935b1db29dfc0
hamed <hamed.kermani1986@gmail.com>
2025-03-22 18:03:57 +0330

    add CMAKE_TOOLCHAIN_FILE and CMAKE_PREFIX_PATH to configure_vcpkg

--- cmake_modules/configure_vcpkg/ConfigureVCPKG.cmake
+++ cmake_modules/configure_vcpkg/ConfigureVCPKG.cmake
@@ -7,6 +7,9 @@ function(configure_vcpkg)
             RESULT_VARIABLE script_result
             #COMMAND_ECHO STDOUT
         )
+        set(CMAKE_TOOLCHAIN_FILE "$ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Path to the toolchain file")
+        set(CMAKE_PREFIX_PATH "$ENV{HOME}/vcpkg/installed/x64-linux" CACHE STRING "Path to the prefix directory") 
+
     elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
         message("Platform is Windows, calling batch file")
         #if both script path and script argument have whitespace, cmake can thandle them with qouting.
@@ -17,6 +20,9 @@ function(configure_vcpkg)
             RESULT_VARIABLE script_result
             #COMMAND_ECHO STDOUT
         )
+        set(CMAKE_TOOLCHAIN_FILE "$ENV{USERPROFILE}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Path to the toolchain file")
+        set(CMAKE_PREFIX_PATH "$ENV{USERPROFILE}/vcpkg/installed/x64-windows" CACHE STRING "Path to the prefix directory")
+
     else()
         message(FATAL_ERROR "Unsupported platform: ${CMAKE_SYSTEM_NAME}")
     endif()
