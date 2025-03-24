dc539bc7cc016e5e19dab7bec864dd9b55896da4
hamed <hamed.kermani1986@gmail.com>
2025-03-24 12:33:45 +0330

    update unit tests

--- tests/node_tests.cpp
+++ tests/node_tests.cpp
@@ -14,26 +14,32 @@ using namespace std;
 class MyTestFixture : public ::testing::Test {
  public:
   void SetUp() override {
-    cout << "\npre test operation executed.";
+    //cout << "\npre test operation executed.";
     // Setup code here
   }
 
   void TearDown() override {
-    cout << "\npost test operation executed.\n";
+    //cout << "\npost test operation executed.\n";
     // Teardown code here
   }
 };
 
 
-TEST_F(MyTestFixture, TestCase1) {
+TEST_F(MyTestFixture, Test_Initial_Message) {
   EXPECT_EQ(Message::buildFirstMessage(3, 5), "00322001234561111111111111111005");
 }
 
-TEST_F(MyTestFixture, TestCase2) {
-  EXPECT_EQ(Message::processMessage(3,"00322001234561111111111111111005",32), "00549701234561111111111111111003");
+TEST_F(MyTestFixture, Test_Process_Message1) {
+  EXPECT_EQ(Message::processMessage(3,"00522101234561111111111111111003",32), "00322201234561111111111111111005");
+}
+TEST_F(MyTestFixture, Test_Process_Message2) {
+  EXPECT_EQ(Message::processMessage(3,"00322101234561111111111111111005",32), "");
+}
+TEST_F(MyTestFixture, Test_Process_Message3) {
+  EXPECT_EQ(Message::processMessage(3,"0052210123456111111111111111003",31), "");
 }
 
-TEST_F(MyTestFixture, TestCase3) {
+TEST_F(MyTestFixture, Test_Id_Message) {
   EXPECT_EQ(Message::buildIdMessage(3), "003");
 }
 
